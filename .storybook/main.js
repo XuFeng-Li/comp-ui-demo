f4c8dd5239acbd2ba99267d43a71824486b9b635
李旭峰 <lxf@asman.com.cn>
2021-02-05 00:49:33 +0800

    ..

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const webpack = require('./webpack.config');
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -7,5 +10,30 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app"
-  ]
-}
+  ],
+  webpackFinal: async (config,{configType}) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader','css-loader','sass-loader'],
+      include: path.resolve(__dirname,'../'),
+    });
+    // config.module.rules.push(webpack.module.rules || {});
+    return config;
+  },
+  babel: async (options) => ({
+    ...options,
+  }),
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      compilerOptions: {
+        allowSyntheticDefaultImports:false,
+        esModuleInterop:false,
+      },
+      // shouldExtractLiteralValuesFromEnum: true,
+      // propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  },
+};
